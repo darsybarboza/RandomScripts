dfc585f397ec5c66154c5d1f6c09ca84214f3aaf
Colton Jackson <53240735+coltonajackson@users.noreply.github.com>
2023-02-25 11:39:56 -0500

    Update guess_game.py - 02/25/2023

--- guess_game.py
+++ guess_game.py
@@ -1,15 +1,19 @@
-done = False
-guesses = []
-answer = "temple"
+print("Welcome to the word guessing game!\n")
+
+answer = "mosiah"
 hint_split = ["_" for c in answer]
 
-while not done:
+guess = ""
+guesses = []
+
+while guess != answer:
     hint = "".join([c for c in hint_split])
     print("Your hint is:", hint)
     guess = input("What is your guess? ")
     guess_split = list(guess)
     answer_split = list(answer)
     if len(guess_split) == len(answer_split):
+        guesses.append(guess)
         for idx, char in enumerate(guess):
             if guess_split[idx] == answer_split[idx]:
                 hint_split[idx] = guess_split[idx]
@@ -17,7 +21,5 @@ while not done:
     else:
         print("Not a valid guess. Please guess again.")
 
-    if guess == hint:
-        done = True
-
-print("You did it!")
+print("Congratulations! You guessed it!")
+print("It took you {0} guesses.".format(len(guesses)))
